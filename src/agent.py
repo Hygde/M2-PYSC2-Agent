203e1637859b39d07230676e1a16486f928a0912
Hygde <samuel.devals@outlook.fr>
2019-03-05 18:06:16 +0100

    Add wrapper_sam

--- src/agent.py
+++ src/agent.py
@@ -1,12 +1,13 @@
-import logging
+import logging, numpy as np
 from pysc2.agents.base_agent import BaseAgent
 from pysc2.lib import actions, features
+from UnitType import TerranUnits
 
 # List of known unit types. It is taken from:
 # https://github.com/Blizzard/s2client-api/blob/master/include/sc2api/sc2_typeenums.h
 
 # How to run:
-# python -m pysc2.bin.agent --agent agent.Agent --map Simple64 --agent_race protoss --use_feature_units True
+# python -m pysc2.bin.agent --agent agent.Agent --map Simple64 --agent_race terran --use_feature_units True
 
 ## This is a protoss agent
 class Agent(BaseAgent):
@@ -18,21 +19,35 @@ class Agent(BaseAgent):
 
     """PRIVATE"""
 
+    # This functions returns True if the action is available
+    # @param action is the action to test
+    # @param obs is all the observation of the current state of the game
+    def __validAction(self, action, obs):
+        return action in obs.observation.available_actions
+
     # This function initializes the root logger and creates _logger attribute
     def __initLogger(self):
         self._logger = logging.getLogger()
         self._logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(module)s :: %(funcName)s :: %(message)s")
+        formatter = logging.Formatter("%(asctime)s :: %(name)s :: %(levelname)s :: %(module)s :: %(funcName)s :: %(message)s")
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
         ch.setFormatter(formatter)
+        self._logger.handlers[0] = ch
 
     """PROTECTED"""
 
-    """PUBLIC"""
+    """PUBLIC"""        
 
     ## This function is called at each step of the game
     # @param obs is all the available observation of the current state of the game
     def step(self, obs):
         super(Agent, self).step(obs)
+        np.set_printoptions(threshold=np.nan)
+        self._logger.debug(self.steps)
+        print(len([unit for unit in obs.observation.feature_units if unit.unit_type == TerranUnits.SCV.value]))
         return actions.FUNCTIONS.no_op()
+
+    ## This function is called to reset the episode
+    def reset(self):
+        super(Agent, self).reset()
---
+++ src/wrapper_sam.py
@@ -0,0 +1,24 @@
+import logging
+from pysc2.lib import actions, features
+from UnitType import TerranUnits
+
+class Sc2Tools:
+
+    def __init__(self, obs):
+        self._logger = logging.getLogger("Sc2ToolsLogger")
+        self._obs = obs
+
+    def _getUnitByType(self, unitType):
+        return [unit for unit in self._obs.observation.feature_units if unit.unit_type == unitType]
+
+    def select(self, unit_type, amount=None, target=None):
+        result = actions.FUNCTIONS.no_op()
+        unit = self._getUnitByType(unit_type)
+        if len(unit) > 0:
+            if not (amount == None) and (amount > 0):unit = unit[:amount]#keep the amount
+        elif not (target == None):
+            if len(target) == 2:result = actions.FUNCTIONS.select()
+            else:pass
+        
+
+    
