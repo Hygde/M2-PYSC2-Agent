a8e983492d65272bbf8cf38215a9b6994cae0833
Hygde <samuel.devals@outlook.fr>
2019-04-18 20:12:20 +0200

    The selection class is completed

--- src/Actions/__init__.py
+++ src/Actions/__init__.py
@@ -1,5 +1,6 @@
 from .sc2action import SC2Action
-from .select_action import SelectAction
+from .select_action import SelectAction, SelectType
 
 __all__ = ["SC2Action",
-           "SelectAction"]
+           "SelectAction"
+           "SelectType"]
--- src/Actions/select_action.py
+++ src/Actions/select_action.py
@@ -1,6 +1,14 @@
 from .sc2action import SC2Action
 from pysc2.lib import features, actions
 
+import numpy as np
+from enum import IntEnum
+
+class SelectType(IntEnum):
+    SINGLE = 0
+    ALL_TYPE = 1
+    ARMY = 2
+
 ## This class defines the way to select N units
 # @param SC2Action is the base class that defines all generalities of SC2 actions
 class SelectAction(SC2Action):
@@ -8,30 +16,31 @@ class SelectAction(SC2Action):
     ## Constructor of SelectAction class
     # @param unit_type is the type of units to select
     # @param n_select defines the number of units to select
-    def __init__(self, unit_type, n_select):
+    def __init__(self, unit_type, kind=None):
         super(SelectAction, self).__init__()
         self._duration = 1
         self._unit_type = unit_type
-        self._n_select = n_select
+        self._kind = kind
 
     def __getUnitsOfType(self, obs, unit_type):
-        return [unit for unit in obs.observation.feature_units if unit.unit_type == unit_type]
+        return np.array([[unit.x, unit.y] for unit in obs.observation.feature_units if unit.unit_type == unit_type and unit.is_selected == False])
 
-    def __select_single(self, coord_yx):
-        coord_xy = [coord_yx.x, coord_yx.y]
+    def __select_single(self, coord_xy):
         return actions.FUNCTIONS.select_point("select", coord_xy)
 
-    def __select_all_type(self, coord_yx):
-        coord_xy = [coord_yx.x, coord_yx.y]
+    def __select_all_type(self, coord_xy):
         return actions.FUNCTIONS.select_point("select_all_type", coord_xy)
+
+    def __select_army(self):return actions.FUNCTIONS.select_army("select")
 
     ## This function performs a selection of N units
     # @param obs defines the observation of the current state of the game
     def action(self, obs):
         result = super(SelectAction, self).action(obs)#return no_op()
+        units = self.__getUnitsOfType(obs, self._unit_type)
         if self._iteration == 0:
-            units = self.__getUnitsOfType(obs, self._unit_type)
-            if self._n_select == 1: result = self.__select_single(units[0])
-            elif self._n_select > 1: pass
-            elif self._n_select == -1:result = self.__select_all_type(units[0])
+            if self._kind == SelectType.SINGLE:result = self.__select_single(units[0])
+            elif self._kind == SelectType.ALL_TYPE:result = self.__select_all_type(units[0])
+            elif self._kind == SelectType.ARMY:result = self.__select_army()
+        self._iteration += 1
         return result
