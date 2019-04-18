4b258e8ad7c8c79a0275054ded882b5ac7069111
Hygde <samuel.devals@outlook.fr>
2019-04-18 20:14:36 +0200

    Adding a comma in the list

--- src/Actions/__init__.py
+++ src/Actions/__init__.py
@@ -2,5 +2,5 @@ from .sc2action import SC2Action
 from .select_action import SelectAction, SelectType
 
 __all__ = ["SC2Action",
-           "SelectAction"
+           "SelectAction",
            "SelectType"]
