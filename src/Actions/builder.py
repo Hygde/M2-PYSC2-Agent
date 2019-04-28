6787f11479064cc882dbb097d5667cb4022423ca
Hygde <samuel.devals@outlook.fr>
2019-04-28 18:56:08 +0200

    Adding an action wich defines the way to build batiments

---
+++ src/Actions/builder.py
@@ -0,0 +1,29 @@
+from .sc2action import SC2Action
+from pysc2.lib import features, actions
+from enum import IntEnum
+
+## enum of building function
+class BFUNCID(IntEnum):
+    SUPPLYDEPOT = actions.FUNCTIONS.Build_SupplyDepot_screen.id
+    BARRACKS = actions.FUNCTIONS.Build_Barracks_screen.id
+    ENGINEERINGBAY = actions.FUNCTIONS.Build_EngineeringBay_screen.id
+    MISSILETURRET = actions.FUNCTIONS.Build_MissileTurret_screen.id
+    BUNKER = actions.FUNCTIONS.Build_Bunker_screen.id
+
+## Defines the way to build a batiment
+class Builder(SC2Action):
+    def __init__(self, to_build, coord_xy, queued=False):
+        self._duration = 1
+        self._to_build = to_build
+        self._queued = self._QUEUED if queued else self._NOT_QUEUED
+        self._coord_xy = coord_xy
+
+    ## This function builds a batiment
+    # @param obs is the current state of the game
+    def action(self, obs):
+        result = super(Builder, self).action(obs)
+        if self._iteration == 0 and self._to_build in self._to_build in obs.observation["available_actions"]:
+            result = actions.FunctionCall(self._to_build, [self._queued, self._coord_xy])
+        else:self._logger.debug("The action isn't available")
+        self._iteration += 1
+        return result
