a93ace40bbdf9dfe70104a5a14ef4f8a7264363c
Hygde <samuel.devals@outlook.fr>
2019-05-14 12:30:45 +0200

    Adding MoveCamera class

---
+++ src/Actions/movecamera.py
@@ -0,0 +1,17 @@
+from .sc2action import SC2Action
+from pysc2.lib import actions
+
+## This class moves the camera to a position
+class MoveCamera(SC2Action):
+    def __init__(self, coord_xy):
+        super(MoveCamera, self).__init__()
+        self._coord_xy = coord_xy
+
+    ## This function move the camera to self._coord_xy
+    # @param obs is the handler of the current state of the game
+    def action(self, obs):
+        result = super(MoveCamera, self).action(obs)
+        if self._iteration == 0 and actions.FUNCTIONS.move_camera.id in obs.observation.available_actions:
+            result = actions.FUNCTIONS.move_camera(self._coord_xy)
+            self._iteration += 1
+        return result
