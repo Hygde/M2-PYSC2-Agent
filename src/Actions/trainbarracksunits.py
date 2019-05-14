fea747794d371171cff6949c887e8c6adb4e610f
Hygde <samuel.devals@outlook.fr>
2019-05-14 21:19:49 +0200

    Remove selector from TrainUnits classes

--- src/Actions/trainbarracksunits.py
+++ src/Actions/trainbarracksunits.py
@@ -4,7 +4,7 @@ from pysc2.lib import units, actions
 class TrainBarracksUnits(_TrainUnits):
 
     def __init__(self, select_type, unit_type, ntrain):
-        super(TrainBarracksUnits, self).__init__(units.Terran.Barracks, select_type, ntrain)
+        super(TrainBarracksUnits, self).__init__(ntrain)
         self._train_func = self._getTrainFunction(unit_type)
 
     def _getTrainFunction(self, unit_type):
@@ -17,12 +17,8 @@ class TrainBarracksUnits(_TrainUnits):
 
     def action(self, obs):
         result = super(TrainBarracksUnits, self).action(obs)
-        if self._iteration == 0:
-            self._logger.debug("self._iteration == 0")
-            result = self._selector.action(obs)
-            if self._selector.isFinished():self._iteration += 1
-        elif self._iteration == 1 and self._train_func in obs.observation["available_actions"]:
-            self._logger.debug("self._iteration == 1")
+        if self._train_func in obs.observation["available_actions"]:
+            self._logger.debug("Adding one unit to training queue")
             result = actions.FunctionCall(self._train_func, [self._NOT_QUEUED])
             self._ntrain -= 1
             if self._ntrain == 0:self._iteration += 1
