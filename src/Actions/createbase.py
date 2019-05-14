738c39adeea6526b9be5487a67ff2f2169454bab
Hygde <samuel.devals@outlook.fr>
2019-05-14 21:21:18 +0200

    Using new version of TrainUnits classes & change bottom positions

--- src/Actions/createbase.py
+++ src/Actions/createbase.py
@@ -13,9 +13,9 @@ class CreateBase(SC2Action):
     def __init__(self, initial_camera_position, top):
         super(CreateBase, self).__init__()
         self._top = top
-        self._duration = 10
+        self._duration = 12
         self._initial_camera_position = initial_camera_position
-        self._act = TrainCommandCenterUnits(1)
+        self._act = SelectAction(units.Terran.CommandCenter, SelectType.SINGLE)
 
     ## This function constructs the base
     # @param obs is the handler of the current state of the game
@@ -23,16 +23,18 @@ class CreateBase(SC2Action):
         result = super(CreateBase, self).action(obs)
         if self._act.isFinished():
             self._iteration += 1
-            if self._iteration == 1:self._act = SelectAction(units.Terran.SCV, SelectType.SINGLE)
-            elif self._iteration == 2:result = self._act = MoveCamera([28.5, 23.5] if self._top else [33.5, 48.5])
-            elif self._iteration == 3:self._act = Builder(BFUNCID.SUPPLYDEPOT, [44,30] if self._top else [12, 28], self._NOT_QUEUED)
-            elif self._iteration == 4:self._act = Builder(BFUNCID.BARRACKS, [43,38] if self._top else [21, 30], self._QUEUED)
-            elif self._iteration == 5:self._act = Builder(BFUNCID.SUPPLYDEPOT, [54,40] if self._top else [22, 38], self._QUEUED)
-            elif self._iteration == 6:self._act = TrainBarracksUnits(SelectType.SINGLE, units.Terran.Marine, 5)
-            elif self._iteration == 7:self._act = SelectAction(units.Terran.SCV, SelectType.SINGLE)            
-            elif self._iteration == 8:result = self._act = MoveCamera(self._initial_camera_position)
+            if self._iteration == 1:self._act = TrainCommandCenterUnits(1)
+            elif self._iteration == 2:self._act = SelectAction(units.Terran.SCV, SelectType.SINGLE)
+            elif self._iteration == 3:self._act = MoveCamera([28.5, 23.5] if self._top else [30.5, 48.5])
+            elif self._iteration == 4:self._act = Builder(BFUNCID.SUPPLYDEPOT, [44,30] if self._top else [28, 28], self._NOT_QUEUED)
+            elif self._iteration == 5:self._act = Builder(BFUNCID.BARRACKS, [43,38] if self._top else [37, 30], self._QUEUED)
+            elif self._iteration == 6:self._act = Builder(BFUNCID.SUPPLYDEPOT, [54,40] if self._top else [39, 39], self._QUEUED)
+            elif self._iteration == 7:self._act = SelectAction(units.Terran.Barracks, SelectType.SINGLE)
+            elif self._iteration == 8:self._act = TrainBarracksUnits(SelectType.SINGLE, units.Terran.Marine, 5)
+            elif self._iteration == 9:self._act = SelectAction(units.Terran.SCV, SelectType.SINGLE)
+            elif self._iteration == 10:result = self._act = MoveCamera(self._initial_camera_position)
             else:
-                minerals =  np.array([[unit.x, unit.y] for unit in obs.observation.feature_units if unit.unit_type == units.Neutral.MineralField])
-                self._act = MoveScreen(minerals[0], True)
+                coord_xy =  np.array([[unit.x, unit.y] for unit in obs.observation.feature_units if unit.unit_type == units.Neutral.MineralField])
+                self._act = MoveScreen(coord_xy[0], True)
         if not self._act.isFinished():result = self._act.action(obs)
         return result
