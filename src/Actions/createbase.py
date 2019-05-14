3c227c196088a05a897a15ce349507f70d948327
Hygde <samuel.devals@outlook.fr>
2019-05-14 12:32:41 +0200

    Using MoveScreen & MoveCamera class

--- src/Actions/createbase.py
+++ src/Actions/createbase.py
@@ -4,6 +4,8 @@ from .sc2action import SC2Action
 from .builder import Builder, BFUNCID
 from .select_action import SelectAction, SelectType
 from .traincommandcenterunits import TrainCommandCenterUnits
+from .movescreen import MoveScreen
+from .movecamera import MoveCamera
 
 class CreateBase(SC2Action):
     def __init__(self, initial_camera_position, top):
@@ -25,15 +27,14 @@ class CreateBase(SC2Action):
         if self._act.isFinished():
             self._iteration += 1
             if self._iteration == 1:self._act = SelectAction(units.Terran.SCV, SelectType.SINGLE)
-            elif self._iteration == 2:result = self._moveCamera()
+            elif self._iteration == 2:result = self._act = MoveCamera([28.5, 23.5] if self._top else [33.5, 48.5])
             elif self._iteration == 3:self._act = Builder(BFUNCID.SUPPLYDEPOT, [44,30] if self._top else [12, 28], self._NOT_QUEUED)
             elif self._iteration == 4:self._act = Builder(BFUNCID.BARRACKS, [43,38] if self._top else [21, 30], self._QUEUED)
             elif self._iteration == 5:self._act = Builder(BFUNCID.SUPPLYDEPOT, [54,40] if self._top else [22, 38], self._QUEUED)
-            elif self._iteration == 6:result = self._moveCamera(self._initial_camera_position)
-            else:pass
-            """minerals =  np.array([[unit.x, unit.y] for unit in obs.observation.feature_units if unit.unit_type == units.Neutral.MineralField])
-            print(minerals)
-            result = actions.FunctionCall("Move_screen", [self._QUEUED, minerals[0]])"""
+            elif self._iteration == 6:result = self._act = MoveCamera(self._initial_camera_position)
+            else:
+                minerals =  np.array([[unit.x, unit.y] for unit in obs.observation.feature_units if unit.unit_type == units.Neutral.MineralField])
+                self._act = MoveScreen(minerals[0])
         if not self._act.isFinished():result = self._act.action(obs)
         #input(self._iteration)
         return result
