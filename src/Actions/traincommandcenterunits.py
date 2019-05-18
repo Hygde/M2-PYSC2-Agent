ff0ae83d8ca95cc42e4960f76b5e915a13f65dd2
Hygde <samuel.devals@outlook.fr>
2019-05-18 10:53:06 +0200

    Add Doxygen comments

--- src/Actions/traincommandcenterunits.py
+++ src/Actions/traincommandcenterunits.py
@@ -2,7 +2,7 @@ from ._trainunits import _TrainUnits
 from .select_action import SelectAction, SelectType
 from pysc2.lib import actions, units
 
-
+## This class defines an easy way to train SCV
 class TrainCommandCenterUnits(_TrainUnits):
 
     __TRAIN_SCV = actions.FUNCTIONS.Train_SCV_quick.id
@@ -10,6 +10,8 @@ class TrainCommandCenterUnits(_TrainUnits):
     def __init__(self, ntrain):
         super(TrainCommandCenterUnits, self).__init__(ntrain)
 
+    ## This function defines the way to create N-SCV
+    # obs is the handler of the current state of the game
     def action(self, obs):
         result = super(TrainCommandCenterUnits, self).action(obs)
         if self.__TRAIN_SCV in obs.observation["available_actions"]:
