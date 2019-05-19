b6d5b1986a890332428dfdf14fad7a392d7d43cb
Hygde <samuel.devals@outlook.fr>
2019-05-19 17:14:04 +0200

    Using _Vespenes class

--- src/Actions/createbase.py
+++ src/Actions/createbase.py
@@ -4,13 +4,14 @@ from .sc2action import SC2Action
 from .select_action import SelectAction, SelectType
 from .TrainUnits import TrainUnits
 from ._createwall import _CreateWall
+from ._vespenes import _Vespenes
 
 ## This class describes the steps to build the base
 class CreateBase(SC2Action):
     def __init__(self, initial_camera_position, top):
         super(CreateBase, self).__init__()
         self._top = top
-        self._duration = 11
+        self._duration = 4
         self._initial_camera_position = initial_camera_position
         self._act = SelectAction(units.Terran.CommandCenter, SelectType.SINGLE)
 
@@ -22,5 +23,6 @@ class CreateBase(SC2Action):
             self._iteration += 1
             if self._iteration == 1:self._act = TrainUnits(units.Terran.SCV, 1)
             elif self._iteration == 2:self._act = _CreateWall(self._initial_camera_position, self._top)
+            elif self._iteration == 3:self._act = _Vespenes()
         if not self._act.isFinished():result = self._act.action(obs)
         return result
