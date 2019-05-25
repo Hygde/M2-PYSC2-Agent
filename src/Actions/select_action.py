b780532f6cb7db8fa5cb5152b70e972ebbaec0fb
Hygde <samuel.devals@outlook.fr>
2019-05-25 16:47:37 +0200

    Add a selection of a idle worker

--- src/Actions/select_action.py
+++ src/Actions/select_action.py
@@ -10,8 +10,10 @@ class SelectType(IntEnum):
     SINGLE = 0
     ## Selects all the units of a type
     ALL_TYPE = 1
+    ## Selects idle worker
+    IDLE_WORKER = 2
     ## Selects the army ignoring the unit type
-    ARMY = 2
+    ARMY = 3
 
 ## This class defines the way to select N units
 # @param SC2Action is the base class that defines all generalities of SC2 actions
@@ -33,6 +35,9 @@ class SelectAction(SC2Action):
     def __select_single(self, coord_xy):
         return actions.FUNCTIONS.select_point("select", coord_xy)
 
+    def __select_idle_worker(self):
+        return actions.FUNCTIONS.select_idle_worker("select")
+
     def __select_all_type(self, coord_xy):
         return actions.FUNCTIONS.select_point("select_all_type", coord_xy)
 
@@ -44,9 +49,12 @@ class SelectAction(SC2Action):
         result = super(SelectAction, self).action(obs)#return no_op()
         if self._units.size == 0:self._units = self.__getUnitsOfType(obs, self._unit_type)
         self._logger.debug(self._units)
-        if self._units.size > 0:
+        if (self._units.size > 0) and (self._kind < SelectType.IDLE_WORKER):
             if self._kind == SelectType.SINGLE:result = self.__select_single(self._units[0])
             elif self._kind == SelectType.ALL_TYPE:result = self.__select_all_type(self._units[0])
+            self._iteration += 1
+        elif self._kind == SelectType.IDLE_WORKER and actions.FUNCTIONS.select_idle_worker.id in obs.observation.available_actions:
+            result = self.__select_idle_worker()
             self._iteration += 1
         elif self._kind == SelectType.ARMY and actions.select_army in obs.observation.available_actions:
             result = self.__select_army()
