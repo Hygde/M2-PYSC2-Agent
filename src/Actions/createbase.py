a398f30df13ee58d63bcc4aacb62a3eda3eee991
Hygde <samuel.devals@outlook.fr>
2019-05-31 11:35:36 +0200

    Using _AdditionalBuildings new version

--- src/Actions/createbase.py
+++ src/Actions/createbase.py
@@ -44,9 +44,9 @@ class CreateBase(SC2Action):
             elif self._iteration == 9:self._act = Builder(BFUNCID.REFINERY, self._geyser_position[1])
             elif self._iteration == 10:self._act = SelectAction(units.Terran.SCV, SelectType.SINGLE, mobs.getUnitsOfType(obs, units.Terran.SCV)[0])
             elif self._iteration == 11:self._act = MoveCamera([28.5, 23.5] if self._top else [30.5, 48.5])
-            elif self._iteration == 12:self._act = Builder(BFUNCID.SUPPLYDEPOT, [24,30] if self._top else [48, 28], self._NOT_QUEUED)
-            elif self._iteration == 13:self._act = Builder(BFUNCID.ENGINEERINGBAY, [23,38] if self._top else [57, 30], self._QUEUED)
-            elif self._iteration == 14:self._act = Builder(BFUNCID.BARRACKS, [10,40] if self._top else [57, 20], self._QUEUED)
+            elif self._iteration == 12:self._act = Builder(BFUNCID.ENGINEERINGBAY, [23,38] if self._top else [57, 30], self._QUEUED)
+            elif self._iteration == 13:self._act = Builder(BFUNCID.BARRACKS, [10,40] if self._top else [57, 20], self._QUEUED)
+            elif self._iteration == 14:self._act = Builder(BFUNCID.SUPPLYDEPOT, [24,30] if self._top else [48, 28], self._NOT_QUEUED)
             elif self._iteration == 15:self._act = Builder(BFUNCID.SUPPLYDEPOT, [34,40] if self._top else [59, 39], self._QUEUED)
             elif self._iteration == 16:self._act = MoveCamera(self._initial_camera_position)
             elif self._iteration == 17:self._act = Harvest(units.Neutral.MineralField, queued=True)
@@ -56,8 +56,8 @@ class CreateBase(SC2Action):
             elif self._iteration == 21:self._act = SelectAction(units.Terran.CommandCenter, SelectType.SINGLE)
             elif self._iteration == 22:self._act = TrainUnits(units.Terran.SCV, 1)
             elif self._iteration == 23:self._act = UseAbility(actions.FUNCTIONS.Morph_OrbitalCommand_quick.id)
-            elif self._iteration == 24:self._act = SelectAction(units.Terran.SCV, SelectType.IDLE_WORKER)
-            elif self._iteration == 25:self._act = MoveCamera([28.5, 23.5] if self._top else [30.5, 48.5])
+            elif self._iteration == 24:self._act = MoveCamera([28.5, 23.5] if self._top else [30.5, 48.5])
+            elif self._iteration == 25:self._act = _AdditionalBuildings(self._initial_camera_position, self._top)
 
         if not self._act.isFinished():result = self._act.action(obs)
         return result
