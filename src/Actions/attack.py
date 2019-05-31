59040540a46995aa67f7520d67f53d2a8111a088
Hygde <samuel.devals@outlook.fr>
2019-05-31 11:36:30 +0200

    Using the 2 barracks to train repears

--- src/Actions/attack.py
+++ src/Actions/attack.py
@@ -13,13 +13,13 @@ class Attack(SC2Action):
         self._top = top
         self._attack_coordinates = [39.5, 45.5] if self._top else [20, 25]
         self._initial_camera_position = initial_camera_position
-        self._act = SelectAction(units.Terran.Barracks, SelectType.SINGLE)
+        self._act = SelectAction(units.Terran.Barracks, SelectType.ALL_TYPE)
 
     def action(self,obs):
         result = super(Attack, self).action(obs)
         if self._act.isFinished():
             self._iteration += 1
-            if self._iteration == 1:self._act = TrainUnits(units.Terran.Reaper, 5)
+            if self._iteration == 1:self._act = TrainUnits(units.Terran.Reaper, 10)
             elif self._iteration == 2:self._act = SelectAction(units.Terran.Reaper, SelectType.ALL_TYPE)
             elif self._iteration == 3:self._act = MoveCamera(self._attack_coordinates)
             elif self._iteration == 4:self._act = MoveScreen([32,32])
