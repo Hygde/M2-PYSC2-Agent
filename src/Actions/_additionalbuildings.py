f29e1be74b9e67cc040c1f748ac4f1b46de50bea
Hygde <samuel.devals@outlook.fr>
2019-05-31 11:34:50 +0200

    Optimizing the construction of the base

--- src/Actions/_additionalbuildings.py
+++ src/Actions/_additionalbuildings.py
@@ -7,6 +7,7 @@ from .movecamera import MoveCamera
 from .builder import Builder, BFUNCID
 from .UseAbility import UseAbility
 from .harvest import Harvest
+from .rallypoint import RallyPoint
 
 class _AdditionalBuildings(SC2Action):
 
@@ -14,37 +15,31 @@ class _AdditionalBuildings(SC2Action):
         super(_AdditionalBuildings, self).__init__()
         self._initial_camera_position = initial_camera_position
         self._top = top
-        self._wall_pos = [28.5, 23.5] if self._top else [30.5, 48.5]
-        self._duration = 24
-        self._act = TrainUnits(units.Terran.SCV, 1)
+        self._duration = 16
+        self._act = SelectAction(units.Terran.EngineeringBay, SelectType.SINGLE)
+
+    def _barracksReady(self, obs):
+        if(obs.observation.multi_select[:,-1] == 0).sum() != 2:self._iteration -=  1
+        else:self._act = RallyPoint(units.Terran.Barracks, ([54,42] if self._top else [28,20]))
 
     def action(self, obs):
         result = super(_AdditionalBuildings, self).action(obs)
         if self._act.isFinished():
             self._iteration += 1
-            if self._iteration == 1:
-                if len(obs.observation.build_queue) != 0:self._iteration -= 1
-                else:self._act = SelectAction(units.Terran.SCV, SelectType.IDLE_WORKER)
-            if self._iteration == 2:self._act = MoveCamera([28.5, 23.5] if self._top else [30.5, 48.5])
-            elif self._iteration == 3:self._act = Builder(BFUNCID.SUPPLYDEPOT, [24,30] if self._top else [48, 28], self._NOT_QUEUED)
-            elif self._iteration == 4:self._act = Builder(BFUNCID.ENGINEERINGBAY, [23,38] if self._top else [57, 30], self._QUEUED)
-            elif self._iteration == 5:self._act = Builder(BFUNCID.SUPPLYDEPOT, [34,40] if self._top else [59, 39], self._QUEUED)
-            elif self._iteration == 6:self._act = Builder(BFUNCID.FACTORY, [10,40] if self._top else [57, 20], self._QUEUED)
-            elif self._iteration == 7:self._act = Builder(BFUNCID.ARMORY, [15,50] if self._top else [60, 49], self._QUEUED)
-            elif self._iteration == 8:self._act = SelectAction(units.Terran.EngineeringBay, SelectType.SINGLE)
-            elif self._iteration == 9:self._act = UseAbility(actions.FUNCTIONS.Research_TerranInfantryArmor_quick.id)
-            elif self._iteration == 10:self._act = UseAbility(actions.FUNCTIONS.Research_TerranInfantryWeapons_quick.id)
-            elif self._iteration == 13:self._act = SelectAction(units.Terran.Barracks, SelectType.SINGLE)
-            elif self._iteration == 14:self._act = TrainUnits(units.Terran.Reaper, 5)
-            elif self._iteration == 15:self._act = SelectAction(units.Terran.SCV, SelectType.IDLE_WORKER)
-            elif self._iteration == 16:self._act = MoveCamera(self._initial_camera_position)
-            elif self._iteration == 17:self._act = Harvest(units.Neutral.MineralField)
-            elif self._iteration == 18:self._act = MoveCamera([28.5, 23.5] if self._top else [30.5, 48.5])
-            elif self._iteration == 19:self._act = SelectAction(units.Terran.Barracks, SelectType.SINGLE)
-            elif self._iteration == 20:self._act = TrainUnits(units.Terran.Reaper, 7)
-            elif self._iteration == 21:self._act = SelectAction(units.Terran.EngineeringBay, SelectType.SINGLE)
-            elif self._iteration == 22:self._act = UseAbility(actions.FUNCTIONS.Research_TerranInfantryArmor_quick.id)
-            elif self._iteration == 23:self._act = UseAbility(actions.FUNCTIONS.Research_TerranInfantryWeapons_quick.id)
-
+            if self._iteration == 1:self._act = UseAbility(actions.FUNCTIONS.Research_TerranInfantryArmor_quick.id)
+            elif self._iteration == 2:self._act = UseAbility(actions.FUNCTIONS.Research_TerranInfantryWeapons_quick.id)
+            elif self._iteration == 3:self._act = SelectAction(units.Terran.SCV, SelectType.IDLE_WORKER)
+            elif self._iteration == 4:self._act = Builder(BFUNCID.MISSILETURRET, [42,48] if self._top else [46,37])
+            elif self._iteration == 5:self._act = Builder(BFUNCID.MISSILETURRET, [35,34] if self._top else [42,21], queued=True)
+            elif self._iteration == 6:self._act = Builder(BFUNCID.FACTORY, [34,21] if self._top else [48, 48], queued=True)
+            elif self._iteration == 7:self._act = SelectAction(units.Terran.Barracks, SelectType.ALL_TYPE)
+            elif self._iteration == 8:self._barracksReady(obs)
+            elif self._iteration == 9:self._act = TrainUnits(units.Terran.Marine, 3)
+            elif self._iteration == 10:self._act = TrainUnits(units.Terran.Reaper, 7)
+            elif self._iteration == 11:self._act = SelectAction(units.Terran.SCV, SelectType.IDLE_WORKER)
+            elif self._iteration == 12:self._act = Builder(BFUNCID.ARMORY, [15,50] if self._top else [60, 49], queued=True)
+            elif self._iteration == 13:self._act = MoveCamera(self._initial_camera_position)
+            elif self._iteration == 14:self._act = Harvest(units.Neutral.MineralField, queued=True)
+            elif self._iteration == 15:self._act = MoveCamera([28.5, 23.5] if self._top else [30.5, 48.5])
         if not self._act.isFinished():result = self._act.action(obs)
         return result
