7751aae9a8957ef7b3b038d341ac94a3375282fc
Hygde <samuel.devals@outlook.fr>
2019-06-01 19:44:19 +0200

    Changing rally point coordinates & Missile Turret position

--- src/Actions/_additionalbuildings.py
+++ src/Actions/_additionalbuildings.py
@@ -20,7 +20,7 @@ class _AdditionalBuildings(SC2Action):
 
     def _barracksReady(self, obs):
         if(obs.observation.multi_select[:,-1] == 0).sum() != 2:self._iteration -=  1
-        else:self._act = RallyPoint(units.Terran.Barracks, ([54,42] if self._top else [28,20]))
+        else:self._act = RallyPoint(units.Terran.Barracks, ([52,48] if self._top else [28,20]))
 
     def action(self, obs):
         result = super(_AdditionalBuildings, self).action(obs)
@@ -29,7 +29,7 @@ class _AdditionalBuildings(SC2Action):
             if self._iteration == 1:self._act = UseAbility(actions.FUNCTIONS.Research_TerranInfantryArmor_quick.id)
             elif self._iteration == 2:self._act = UseAbility(actions.FUNCTIONS.Research_TerranInfantryWeapons_quick.id)
             elif self._iteration == 3:self._act = SelectAction(units.Terran.SCV, SelectType.IDLE_WORKER)
-            elif self._iteration == 4:self._act = Builder(BFUNCID.MISSILETURRET, [42,48] if self._top else [46,37])
+            elif self._iteration == 4:self._act = Builder(BFUNCID.MISSILETURRET, [36,46] if self._top else [46,37])
             elif self._iteration == 5:self._act = Builder(BFUNCID.MISSILETURRET, [35,34] if self._top else [42,21], queued=True)
             elif self._iteration == 6:self._act = Builder(BFUNCID.FACTORY, [34,21] if self._top else [48, 48], queued=True)
             elif self._iteration == 7:self._act = SelectAction(units.Terran.Barracks, SelectType.ALL_TYPE)
