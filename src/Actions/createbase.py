20fedfda0cea2b401fcc84152cdde22e0b4d2672
GGOUSSEAUD <gousseaud.gaetan.pro@gmail.com>
2019-06-11 02:48:08 +0200

    Ajout du fix vespene Linux, toujours un problème au niveau du harvest

--- src/Actions/createbase.py
+++ src/Actions/createbase.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pysc2.lib import units, actions
+from pysc2.lib import units, actions, features
 from .sc2action import SC2Action
 from .select_action import SelectAction, SelectType
 from .TrainUnits import TrainUnits
@@ -10,6 +10,8 @@ from .builder import Builder, BFUNCID
 from .movecamera import MoveCamera
 from .harvest import Harvest
 from .UseAbility import UseAbility
+import math
+import random
 from .rallypoint import RallyPoint
 from Tools import Observations as mobs
 
@@ -26,8 +28,11 @@ class CreateBase(SC2Action):
     # @param obs is the handler of the current state of the game
     def action(self, obs):
         result = super(CreateBase, self).action(obs)
-
+        _UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
+        print(self._iteration)
+        #input("before isfinished")
         if self._act.isFinished():
+            #input("IsFinished")
             self._iteration += 1
             if self._iteration == 1:self._act = TrainUnits(units.Terran.SCV, 1)
             elif self._iteration == 2:self._act = _CreateWall(self._initial_camera_position, self._top)
@@ -36,12 +41,33 @@ class CreateBase(SC2Action):
                 self._geyser_position = mobs.getUnitsOfType(obs, units.Neutral.VespeneGeyser)
                 pos = np.array([[unit.x, unit.y] for unit in obs.observation.feature_units if unit.unit_type == units.Terran.CommandCenter])
                 pos += (np.array([10,0]) if self._top else np.array([-10,-10]))
+                #input("Before rally point")
                 self._act = RallyPoint(units.Terran.CommandCenter, pos[0], queued=False)
-            elif self._iteration == 5:self._act = TrainUnits(units.Terran.SCV, 5)
-            elif self._iteration == 6:self._act = SelectAction(units.Terran.SCV, SelectType.SINGLE)
-            elif self._iteration == 7:self._act = Builder(BFUNCID.REFINERY, self._geyser_position[0], queued=True)
+                #input("After rally point")
+            elif self._iteration == 5:
+                self._act = TrainUnits(units.Terran.SCV, 5)
+                #input("After train units")
+            elif self._iteration == 6:
+                self._act = SelectAction(units.Terran.SCV, SelectType.SINGLE)
+                #input("After select action")
+            elif self._iteration == 7:
+                print(obs.observation.feature_units)
+                #input("After print obs")
+                self.unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
+                self.vespene_y, self.vespene_x = (self.unit_type == units.Neutral.VespeneGeyser).nonzero()
+                self.vespene_geyser_count = int(math.ceil(len(self.vespene_y) / 97))
+                rand = random.randint(0, abs(len(self.vespene_y) - 1))
+                target = [int(self.vespene_x[rand]), int(self.vespene_y[rand])]
+                self._act = Builder(BFUNCID.REFINERY, target, queued=True)
+                #input("After Building ref")
             elif self._iteration == 8:self._act = SelectAction(units.Terran.SCV, SelectType.SINGLE, mobs.getUnitsOfType(obs, units.Terran.SCV)[0])
-            elif self._iteration == 9:self._act = Builder(BFUNCID.REFINERY, self._geyser_position[1])
+            elif self._iteration == 9:
+                self.unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
+                self.vespene_y, self.vespene_x = (self.unit_type == units.Neutral.VespeneGeyser).nonzero()
+                self.vespene_geyser_count = int(math.ceil(len(self.vespene_y) / 97))
+                rand = random.randint(0, abs(len(self.vespene_y) - 1))
+                target = [int(self.vespene_x[rand]), int(self.vespene_y[rand])]
+                self._act = Builder(BFUNCID.REFINERY, target)
             elif self._iteration == 10:self._act = SelectAction(units.Terran.SCV, SelectType.SINGLE, mobs.getUnitsOfType(obs, units.Terran.SCV)[0])
             elif self._iteration == 11:self._act = MoveCamera([28.5, 23.5] if self._top else [30.5, 48.5])
             elif self._iteration == 12:self._act = Builder(BFUNCID.ENGINEERINGBAY, [23,38] if self._top else [57, 30], self._QUEUED)
